0244b01aeb2d9e88ef9f4e73ceb1194968c43adc
Hanashi <hanashi@hanashi.eu>
2019-01-28 21:55:51 +0100

    Exception wird geloggt wenn Debugmodus aktiv ist

--- files/lib/system/exception/TeamSpeakException.class.php
+++ files/lib/system/exception/TeamSpeakException.class.php
@@ -9,4 +9,17 @@ namespace wcf\system\exception;
 * @license	Freie Lizenz (https://hanashi.eu/freie-lizenz/)
 * @package	WoltLabSuite\Core\System\Exception
 */
-class TeamSpeakException extends SystemException {}
+class TeamSpeakException extends SystemException {
+    /**
+	 * Creates a new TeamSpeakException.
+	 * 
+	 * @param	string		$message	error message
+	 */
+    public function __construct($message = '') {
+        parent::__construct((string)$message);
+
+        if (ENABLE_DEBUG_MODE) {
+            \wcf\functions\exception\logThrowable($this);
+        }
+    }
+}
