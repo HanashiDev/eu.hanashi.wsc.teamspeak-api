3b343b8f5d31e2b611f5c93f41f53b1159ce324e
Peter Lohse <peterlohse@yahoo.de>
2019-02-11 07:38:21 +0100

    Raw LibSSh2 hinzugefügt

--- files/lib/system/teamspeak/TeamSpeakConnectionHandler.class.php
+++ files/lib/system/teamspeak/TeamSpeakConnectionHandler.class.php
@@ -38,7 +38,11 @@ class TeamSpeakConnectionHandler {
         if ($queryProtocol == 'raw') {
             $this->teamSpeakHandler = new TeamSpeakRawHandler($hostname, $port, $username, $password);
         } else if ($queryProtocol == 'ssh') {
-            $this->teamSpeakHandler = new TeamSpeakSshHandler($hostname, $port, $username, $password);
+            if (function_exists('ssh2_connect') && function_exists('ssh2_auth_password') && function_exists('ssh2_shell')) {
+                $this->teamSpeakHandler = new TeamSpeakLibSsh2Handler($hostname, $port, $username, $password);
+            } else {
+                $this->teamSpeakHandler = new TeamSpeakSshHandler($hostname, $port, $username, $password);
+            }
         }
     }
     
---
+++ files/lib/system/teamspeak/TeamSpeakLibSsh2Handler.class.php
@@ -0,0 +1,138 @@
+<?php
+namespace wcf\system\teamspeak;
+use wcf\system\exception\TeamSpeakException;
+use wcf\util\StringUtil;
+use wcf\util\TeamSpeakUtil;
+
+/**
+* Api for connection with TeamSpeak ssh server query with libssh2.
+*
+* @author	Peter Lohse <hanashi@hanashi.eu>
+* @copyright	Hanashi
+* @license	Freie Lizenz (https://hanashi.eu/freie-lizenz/)
+* @package	WoltLabSuite\Core\System\TeamSpeak
+*/
+class TeamSpeakLibSsh2Handler implements ITeamSpeakHandler {
+    /**
+     * the hostname/ip of your TeamSpeak server
+     * 
+     * @var string
+     */
+    protected $hostname;
+
+    /**
+     * the server query port of your TeamSpeak server (standard: raw = 10011; ssh = 10022)
+     * 
+     * @var int
+     */
+    protected $port;
+
+    /**
+     * Username of server query (standard: serveradmin)
+     * 
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * Password of server query
+     * 
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * SSH2 connection ressource
+     * 
+     * @var resource
+     */
+    protected $connection;
+
+    /**
+     * server query object
+     * 
+     * @var resource
+     */
+    protected $queryObj;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct($hostname, $port, $username, $password) {
+        $this->hostname = $hostname;
+        $this->port = $port;
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->connect();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __destruct() {
+        if ($this->queryObj) {
+            $this->execute('quit');
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function connect() {
+        $this->connection = ssh2_connect($this->hostname, $this->port);
+
+        if (!$this->connection) {
+            throw new TeamSpeakException('Connection failed');
+        }
+
+        // login to server query
+        $this->login($this->username, $this->password);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function login($username, $password) {
+        if (!ssh2_auth_password($this->connection, $username, $password)) {
+            throw new TeamSpeakException('Authentication failed...');
+        }
+        $this->queryObj = ssh2_shell($this->connection, 'raw');
+        if (!$this->queryObj) {
+            throw new TeamSpeakException('Opening Shell failed');
+        }
+        stream_set_blocking($this->queryObj, true);
+
+        $header = StringUtil::trim($this->read());
+        if ($header != 'TS3') {
+            throw new TeamSpeakException('Not a TeamSpeak server');
+        }
+        $motd = StringUtil::trim($this->read());
+    }
+
+    public function read() {
+        return stream_get_line($this->queryObj, PHP_INT_MAX, "\n\r");
+    }
+
+    public function write($line) {
+        fwrite($this->queryObj, $line . "\n");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute($command) {
+        $result = [];
+        $this->write($command);
+        $commandLine = $this->read();
+        if ($command == 'quit') {
+            return [];
+        }
+        do {
+            $line = StringUtil::trim($this->read());
+            $result[] = $line;
+        } while ($line && substr($line, 0, 5) != "error");
+        
+        return $result;
+    }
+}
