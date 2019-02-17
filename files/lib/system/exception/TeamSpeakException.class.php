fbae5700054100abfd09fc5353a0d452e675df79
Hanashi <hanashi@hanashi.eu>
2019-02-17 20:29:59 +0100

    Debug Müll entfernt

--- files/lib/system/exception/TeamSpeakException.class.php
+++ files/lib/system/exception/TeamSpeakException.class.php
@@ -9,18 +9,4 @@ namespace wcf\system\exception;
 * @license	Freie Lizenz (https://hanashi.eu/freie-lizenz/)
 * @package	WoltLabSuite\Core\System\Exception
 */
-class TeamSpeakException extends SystemException {
-    /**
-	 * Creates a new TeamSpeakException.
-	 * 
-	 * @param	string		$message	error message
-	 */
-    public function __construct($message = '') {
-        parent::__construct((string)$message);
-
-        // wenn Debug Modus aktiv wird exception geloggt
-        if (ENABLE_DEBUG_MODE) {
-            $this->getExceptionID();
-        }
-    }
-}
+class TeamSpeakException extends SystemException {}
