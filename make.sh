1daafdfbf41564d698e4871e362c256deab8dd9a
xXSchrandXx <marvinschrand@yahoo.de>
2021-11-10 15:49:22 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PACKAGE_NAME=eu.hanashi.wsc.minecraft-api
+PACKAGE_NAME=de.xxschrandxx.wsc.minecraft-api
 PACKAGE_TYPES=(acptemplates files)
 
 rm -rf files/js/*
@@ -12,7 +12,7 @@ do
 done
 
 rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
-7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.github
+7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!phpcs.xml -x!Readme.md -x!pictures
 7z a ${PACKAGE_NAME}.tar.gz ${PACKAGE_NAME}.tar
 rm -rf ${PACKAGE_NAME}.tar
 
