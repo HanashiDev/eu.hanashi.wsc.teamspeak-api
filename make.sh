efe5df2476a40475e615c6e36f36fe96b9f867c1
xXSchrandXx <marvinschrand@yahoo.de>
2021-11-23 14:37:11 +0100

    Remove \r

--- make.sh
+++ make.sh
@@ -2,22 +2,16 @@
 PACKAGE_NAME=de.xxschrandxx.wsc.minecraft-api
 PACKAGE_TYPES=(acptemplates files)
 
-rm -rf files/js/*
-tsc --build
-
 for i in "${PACKAGE_TYPES[@]}"
 do
     rm -rf ${i}.tar
-    7z a -ttar -mx=9 ${i}.tar ./${i}/*
+    tar -c ${i}.tar ./${i}/*
 done
 
-rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
-7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!phpcs.xml -x!Readme.md -x!pictures
-7z a ${PACKAGE_NAME}.tar.gz ${PACKAGE_NAME}.tar
 rm -rf ${PACKAGE_NAME}.tar
+tar -c ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!phpcs.xml -x!Readme.md -x!pictures
 
 for i in "${PACKAGE_TYPES[@]}"
 do
     rm -rf ${i}.tar
 done
-
