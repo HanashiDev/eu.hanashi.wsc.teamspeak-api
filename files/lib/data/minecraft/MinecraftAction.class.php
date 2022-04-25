9b1a0608fccb06dab68f19a181142d141ac31312
xXSchrandXx <marvinschrand@yahoo.de>
2022-04-25 13:49:15 +0200

    Change errorstring into array

--- files/lib/data/minecraft/MinecraftAction.class.php
+++ files/lib/data/minecraft/MinecraftAction.class.php
@@ -5,12 +5,11 @@ namespace wcf\data\minecraft;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
 use wcf\data\AbstractDatabaseObjectAction;
-use wcf\system\exception\MinecraftException;
 use wcf\system\exception\PermissionDeniedException;
 use wcf\system\exception\SystemException;
-use wcf\system\registry\RegistryHandler;
 use wcf\system\WCF;
 use wcf\util\DateUtil;
+use wcf\util\JSON;
 
 /**
  * Minecraft Action class
@@ -50,27 +49,6 @@ class MinecraftAction extends AbstractDatabaseObjectAction
     /**
      * @inheritDoc
      */
-    public function delete()
-    {
-        parent::delete();
-
-        foreach ($this->getObjects() as $object) {
-            $url = $object->url;
-            $minecraftList = new MinecraftList();
-            $minecraftList->getConditionBuilder()->add('url = ?', [$url]);
-            $minecraftList->readObjectIDs();
-            if (empty($minecraftList->getObjectIDs())) {
-                $regHandler = RegistryHandler::getInstance();
-                $package = "de.xxschrandxx.wsc.minecraft-api";
-                $field = "minecraftFloodgate-$url";
-                $regHandler->delete($package, $field);
-            }
-        }
-    }
-
-    /**
-     * @inheritDoc
-     */
     public function update()
     {
         if (isset($this->parameters['data']['password']) && empty($this->parameters['data']['password'])) {
@@ -78,19 +56,6 @@ class MinecraftAction extends AbstractDatabaseObjectAction
         }
 
         parent::update();
-
-        foreach ($this->getObjects() as $object) {
-            $url = $object->url;
-            $minecraftList = new MinecraftList();
-            $minecraftList->getConditionBuilder()->add('url = ?', [$url]);
-            $minecraftList->readObjectIDs();
-            if (empty($minecraftList->getObjectIDs())) {
-                $regHandler = RegistryHandler::getInstance();
-                $package = "de.xxschrandxx.wsc.minecraft-api";
-                $field = "minecraftFloodgate-$url";
-                $regHandler->delete($package, $field);
-            }
-        }
     }
 
     /**
@@ -106,6 +71,7 @@ class MinecraftAction extends AbstractDatabaseObjectAction
         }
     }
 
+    // TODO turn into html
     /**
      * Checking status of server.
      * @return array
@@ -118,30 +84,42 @@ class MinecraftAction extends AbstractDatabaseObjectAction
                 $minecraft = new Minecraft($minecraftID);
                 $handler = $minecraft->getConnection();
                 $response = $handler->call('GET');
-// TODO turn into html
-//                $responseBody = JSON::decode($response->getBody());
                 $responseBody = $response->getBody()->getContents();
-                $responses[$minecraftID] = $responseBody;
+                $responses[$minecraftID] = JSON::decode($responseBody);
             } catch (ClientException $e) {
                 switch ($e->getCode()) {
-                    case 401:
-                        $responses[$minecraftID] = "{'status': '" . $e->getMessage() . "', 'statusCode': " . $e->getCode() . "}";
-                        break;
                     case 429:
                         if ($e->hasResponse()) {
                             $time = DateUtil::getDateTimeByTimestamp(TIME_NOW + (int) $e->getResponse()->getHeaderLine('Retry-After'));
-                            $responses[$minecraftID] = "{'status': '" . $e->getMessage() . " wait until " . DateUtil::format($time, DateUtil::TIME_FORMAT) . "', 'statusCode': " . $e->getCode() . "}";
+                            $responses[$minecraftID] = [
+                                'status' => $e->getMessage() . " wait until " . DateUtil::format($time, DateUtil::TIME_FORMAT),
+                                'statusCode' => $e->getCode()
+                            ];
                         } else {
-                            $responses[$minecraftID] = "{'status': '" . $e->getMessage() . "', 'statusCode': " . $e->getCode() . "}";
+                            $responses[$minecraftID] =  [
+                                'status' => $e->getMessage(),
+                                'statusCode' => $e->getCode()
+                            ];
                         }
                         break;
                     default:
-                        $responses[$minecraftID] = "{'status': '" . $e->getMessage() . "', 'statusCode': " . $e->getCode() . "}";
+                        $responses[$minecraftID] = [
+                            'status' => $e->getMessage(),
+                            'statusCode' => $e->getCode()
+                        ];
                         break;
                 }
             } catch (GuzzleException $e) {
-                $responses[$minecraftID] = "{'status': '" . $e->getMessage() . "', 'statusCode': " . $e->getCode() . "}";
-            }
+                $responses[$minecraftID] = [
+                    'status' => $e->getMessage(),
+                    'statusCode' => $e->getCode()
+                ];
+            } catch (SystemException $e) {
+                $responses[$minecraftID] = [
+                    'status' => $e->getMessage(),
+                    'statusCode' => $e->getCode()
+                ];
+            } 
         }
         return $responses;
     }
