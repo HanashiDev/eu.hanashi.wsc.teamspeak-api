8950c39ff8dcd7d353f3b2bcb9abadff708c68b3
xXSchrandXx <marvinschrand@yahoo.de>
2022-04-27 15:37:20 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -14,6 +14,7 @@ done
 rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
 7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!phpcs.xml -x!Readme.md -x!pictures -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json -x!ts -x!constants.php
 7z a ${PACKAGE_NAME}.tar.gz ${PACKAGE_NAME}.tar
+rm -rf ${PACKAGE_NAME}.tar
 
 for i in "${PACKAGE_TYPES[@]}"
 do
