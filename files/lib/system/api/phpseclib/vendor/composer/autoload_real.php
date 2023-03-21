5b9c108072d654024020f923a2db770e44df57f6
Peter Lohse <hanashi@hanashi.eu>
2023-03-21 11:46:33 +0100

    fix CVE-2023-27560

--- files/lib/system/api/phpseclib/vendor/composer/autoload_real.php
+++ files/lib/system/api/phpseclib/vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit4ab289022e37aa8698f8c3072b619243
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit4ab289022e37aa8698f8c3072b619243::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire4ab289022e37aa8698f8c3072b619243($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit4ab289022e37aa8698f8c3072b619243::$files;
+        $requireFile = static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        };
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            ($requireFile)($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire4ab289022e37aa8698f8c3072b619243($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
